a28d45c8932543aaa5ae0499b39762a1edc21ead
Dmitry Boyarintsev <skalogryz.lists@gmail.com>
2021-02-09 02:11:18 -0500

    adding a background for a better macOS installation experience

--- install/macOS/settings.py
+++ install/macOS/settings.py
@@ -3,6 +3,7 @@ symlinks = { 'Applications': '/Applications' }
 icon_locations = {
     'Working Memory Analyzer.app': (100, 100),
     'Applications': (300, 100),
-    'Experiment Library': (150, 300)
+    'Experiment Library': (100, 300)
 }
 window_rect = ((40, 40), (420,450))
+background = './background.png'
