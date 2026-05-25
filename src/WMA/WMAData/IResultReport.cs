eb26c13e22c6ca93ec77570317d0f0e54e9569d1
skalogryz <skalogryz.lists@gmail.com>
2026-05-25 15:35:10 -0400

    adding result report interface

---
+++ src/WMA/WMAData/IResultReport.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WMAData
+{
+    public interface IResultReport
+    {
+        // Write Date
+        // Write Time
+        // Write Particiap ID
+        // Write Age
+        // Write Gender
+
+        void WriteConfig(Configuration cfg);
+    }
+}
---
+++ src/WMA/WMAData/ResultReport.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WMAData
+{
+    public class ResultReport : IResultReport
+    {
+        public StringBuilder text = new StringBuilder();
+
+        public void WriteConfig(Configuration cfg)
+        {
+
+        }
+    }
+}
