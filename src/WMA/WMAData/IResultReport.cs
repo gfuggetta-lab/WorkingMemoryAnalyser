fe84a6af6d40e87480a8e0f250a3722996693a74
skalogryz <skalogryz.lists@gmail.com>
2026-05-25 19:51:24 -0400

    resetting time

--- src/WMA/WMAData/IResultReport.cs
+++ src/WMA/WMAData/IResultReport.cs
@@ -23,6 +23,8 @@ namespace WMAData
         // -1: not given; 0: incorrect; 1: correct
         public int observedDataCorrectResponseRecord;
 
+        // record Reaction Time  between response event
+        // and time t1 taken immediately after s4 render command is sent;
         public int responseTimeMs;
 
         public int s1_onsetTime;
@@ -35,6 +37,23 @@ namespace WMAData
         public int TMS_onsetTime;
 
         public int isRuinedTrial;
+
+        public void Reset()
+        {
+            observedDataResponseRecord = -1;
+            observedDataCorrectResponseRecord = -1;
+            responseTimeMs = -1;
+            s1_onsetTime = -1;
+            s2_onsetTime = -1;
+            s3_onsetTime = -1;
+            s4_onsetTime = -1;
+            response_onsetTime = -1;
+            feedback_onsetTime = -1;
+            blank_onsetTime = -1;
+
+            TMS_onsetTime = -1;
+            isRuinedTrial = 0;
+        }
     }
 
 }
