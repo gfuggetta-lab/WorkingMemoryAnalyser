13b9b0af50e22e7561ff1eecae82ba4292ce9684
skalogryz <skalogryz.lists@gmail.com>
2026-05-25 20:05:55 -0400

    adding the experiment data

---
+++ src/WMA/WMAData/ExperimentData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WMAData
+{
+    public class ExperimentData
+    {
+        public string ExperimentName;
+        public string ParticipantId;
+        public string Age;
+        public string Sex;
+        public DateTime TimeStamp;
+        public string Handedness;
+        public string DisplayType;
+
+        public ExperimentData()
+        {
+
+        }
+
+        public static ExperimentData Start()
+        {
+            return new ExperimentData
+            {
+                TimeStamp = DateTime.Now
+            };
+        }
+    }
+}
