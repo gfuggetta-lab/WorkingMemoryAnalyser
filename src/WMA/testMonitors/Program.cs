2da9106ce1b6fc59fd9ba8f60b313589a8a8f86b
skalogryz <skalogryz.lists@gmail.com>
2026-05-30 22:13:38 -0400

    testing monitor information

---
+++ src/WMA/testMonitors/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using MonitorInfo;
+
+namespace testMonitors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var en = MonitorEnumerator.CreateEnumerator();
+            if (en == null)
+            {
+                Console.WriteLine("no enumarators available");
+                return;
+            }
+            foreach(var m in en.GetConnectedMonitors())
+            {
+                Console.WriteLine($"{m.Id} {m.Name} {m.NativeDeviceId}");
+            }
+        }
+    }
+}
