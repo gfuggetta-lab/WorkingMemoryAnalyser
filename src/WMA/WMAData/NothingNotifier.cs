f3c1478a0639ae98829014d623cdb121e3ab5f79
skalogryz <skalogryz.lists@gmail.com>
2026-06-15 16:29:55 -0400

    adding empty notifier

---
+++ src/WMA/WMAData/NothingNotifier.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WMAData
+{
+    public class NothingNotifier : IAsyncExperimentNotifier
+    {
+        public Task StartExperiment()
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task MarkerS1(int marker)
+        {
+            return Task.CompletedTask;
+        }
+        public Task MarkerS2(int marker)
+        {
+            return Task.CompletedTask;
+        }
+        public Task MarkerS3(int marker)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task MarkerS4(int marker)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task Feedback(bool isCorrect)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
