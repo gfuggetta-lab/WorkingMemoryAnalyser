00cdd6a2cbe8d6cf782aa7ec9da8aa524ab875d6
skalogryz <skalogryz.lists@gmail.com>
2026-06-15 12:22:44 -0400

    adding the combobox dropdown size limit. For the smaller comboboxes

---
+++ src/godot/Scripts/PopupSizeLimit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Godot;
+
+namespace godotutils
+{
+    public partial class PopupSizeLimit: OptionButton
+    {
+        [Export]
+        public int MaxHeight = 0;
+        public override void _Ready()
+        {
+            var popup = this.GetPopup();
+            if (popup == null)
+            {
+                GD.Print("no popup");
+                return;
+            }
+            if (MaxHeight > 0)
+            {
+                var sz = popup.MaxSize;
+                sz.Y = MaxHeight;
+                popup.MaxSize = sz;
+            }
+        }
+    }
+}
