6c7b0814b9bf170607f4743cd535c0c778a75444
skalogryz <skalogryz.lists@gmail.com>
2026-07-12 12:58:22 -0400

    even more abstractions

---
+++ src/WMA/WMAData/IDataProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WMAData
+{
+    // The interface that allows to gain access
+    // to the experiment information and resources
+    public interface IDataProvider
+    {
+        Task<bool> IsConfigureFile(string filename);
+        IConfigReader GetReader(string filename);
+    }
+}
