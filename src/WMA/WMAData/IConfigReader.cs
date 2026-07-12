4faf7bbf487b3f2cf7de34d4ea3b2ea99f70c9bc
skalogryz <skalogryz.lists@gmail.com>
2026-07-12 12:50:24 -0400

    starting the abstraction for reading the configuration file

---
+++ src/WMA/WMAData/IConfigReader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WMAData
+{
+    // The interface to implement the configuration reader
+    public interface IConfigReader
+    {
+        Task<bool> ReadConfig(Configuration cfg, CancellationToken cancel);
+    }
+}
---
+++ src/WMA/WMAFiles/TextFileReader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WMAData;
+
+namespace WMAFiles
+{
+    // This is the legacy .txt format to read the configuration
+    public class TextFileReader : IConfigReader
+    {
+        public string configFileName;
+        public Task<bool> ReadConfig(Configuration cfg, CancellationToken cancel)
+        {
+            ConfigFile file = ConfigFile.FromFile(configFileName);
+            cfg.LoadConfig(file);
+            return Task.FromResult(true);
+        }
+    }
+}
